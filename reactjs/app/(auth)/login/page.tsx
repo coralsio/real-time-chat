c926a67d939080677545051d6acc19b2bcd16860
saeed <saeed@corals.io>
2024-08-30 18:03:46 +0300

    updates

--- reactjs/app/(auth)/login/page.tsx
+++ reactjs/app/(auth)/login/page.tsx
@@ -12,7 +12,7 @@ export default function Login(): JSX.Element {
         <section className="bg-gray-50 dark:bg-gray-900">
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                 <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-                    <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+                    <img className="w-8 h-8 mr-2" src="https://www.laraship.com/wp-content/uploads/2017/12/favicon.png"
                          alt="logo"/>
                     {process.env.NEXT_PUBLIC_APP_NAME}
                 </a>
